2a1c7eee52d4301faf20dad044ec09b62813d222
Gregory Lureau <glureau@deezer.com>
2022-03-26 11:49:08 +0100

    init

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,29 @@
+@file:OptIn(ExperimentalJsExport::class)
+
+val dynamicNull: dynamic = null
+
+class NonExported {
+    val goo = 12
+}
+
+@JsExport
+class Foo(data1: String, data2: String){
+    internal lateinit var common: NonExported
+
+    init {
+        if (data1 != dynamicNull) {
+            common = NonExported()
+        }
+    }
+
+    val exposedValue: Int
+        get() = common.goo
+
+    internal constructor(common: NonExported): this(dynamicNull?.dynamicCastTo<String>(), dynamicNull?.dynamicCastTo<String>()) {
+        this.common = common
+    }
+}
+
+@JsExport
+fun buildTestingKT() = Foo(NonExported()) // internal constructor
+
